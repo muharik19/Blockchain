73e2a32613be6167a5ddc88fed1d0b3540806e29
muharik19 <ahmadmuharik@gmail.com>
2022-09-24 22:58:45 +0700

    unit test

--- brownie_fund_me/scripts/helpful_scripts.py
+++ brownie_fund_me/scripts/helpful_scripts.py
@@ -1,16 +1,21 @@
 from brownie import network, config, accounts, MockV3Aggregator
 
-
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 DECIMALS = 8
 STARTING_PRICE = 200000000000
 
 
-def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
-    else:
+    if id:
         return accounts.add(config["wallets"]["from_key"])
 
 
